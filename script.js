5c90df06a0530e30f06b0260402b951db1796f48
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-16 20:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 //complete this code
 class Rectangle {
-	 constructor(width, height) {
-        this._width = width;
-        this._height = height;
-    }
+	constructor(width ,height)
+	{
+		this.width=width;
+		this.height=height;
+	}
 
-    get width() {
-        return this._width;
-    }
+	get getWidth()
+	{
+		return this.width;
+	}
 
-    get height() {
-        return this._height;
-    }
+	get getHeight()
+	{
+		return this.height;
+	}
 
-    getArea() {
-        return this._width * this._height;
-    }
+	getArea()
+	{
+		return (this.width) * (this.height);
+	}
 }
 
 class Square extends Animal {
